2df14381f4f144068cf192c1061dd4acaee21465
Xiaoyuu <xandcosmos@gmail.com>
2019-11-26 12:16:20 +0800

    Initial Commit

---
+++ LearningiOS/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LearningiOS
+//
+//  Created by 张啸宇 on 2019/11/26.
+//  Copyright © 2019 xiaoyuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ LearningiOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LearningiOS
+//
+//  Created by 张啸宇 on 2019/11/26.
+//  Copyright © 2019 xiaoyuu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
